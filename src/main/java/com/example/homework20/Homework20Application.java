acd655e5fa0479c9bc209e18a8ac5e5e61d827e3
violoniste <violoniste2@gmail.com>
2023-10-26 22:46:29 +0700

    Initial

---
+++ src/main/java/com/example/homework20/Homework20Application.java
@@ -0,0 +1,13 @@
+package com.example.homework20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homework20/Homework20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
